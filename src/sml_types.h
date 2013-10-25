25a23d0363ca5c08fcebdfdcc152f2e79a6228d6
Wei Yao <ywsample@gmail.com>
2013-10-25 10:15:40 +0800

    Update sml_types.h
    fix a error in define u_char

--- src/sml_types.h
+++ src/sml_types.h
@@ -1,6 +1,8 @@
 #ifndef _SML_TYPES_H_INCLUDED_
 #define _SML_TYPES_H_INCLUDED_
 
-typedef unsigned char * u_char
+typedef unsigned char u_char
 typedef unsigned int sml_uint
 typedef int sml_int
+
+#endif
